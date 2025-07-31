8f666113eed76fe69dd2a9d2affffd02eb24829d
dbochoa77 <dbochoa77@proton.me>
2025-07-31 15:40:35 -0700

    updated hardware-configuration

--- nixosBase/hosts/nixos/hardware-configuration.nix
+++ nixosBase/hosts/nixos/hardware-configuration.nix
@@ -13,6 +13,19 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/f2dc1a58-84af-4777-93d6-1f16ace67e2c";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/D244-BDA2";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction

ce69fac2509f1f53a645cd3e34d157a4ebbf1f2d
dbochoa77 <dbochoa77@proton.me>
2025-08-11 14:59:20 -0700

    make network private

--- nixosBase/hosts/nixos/services/containers/httpd/default.nix
+++ nixosBase/hosts/nixos/services/containers/httpd/default.nix
@@ -2,11 +2,15 @@
   containers.httpd = { 
     autoStart = true;
     ephemeral = true;
+    privateNetwork = true;
+    hostAddress = "192.168.122.1";
+    localAddress = "192.168.122.130";
     config = {...}: {
      services.httpd = {
       enable = true;
       adminAddr = "foo@example.org";
-        
+     };
+    };   
     bindMounts = {
         "/root/data" = {
           hostPath = "/home/admin/data";
@@ -14,6 +18,4 @@
          };
        };
      };
-   };
-  };
 }

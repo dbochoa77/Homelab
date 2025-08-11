9e77abf818ff128b81153a666257712e723ab139
dbochoa77 <dbochoa77@proton.me>
2025-08-11 13:51:16 -0700

    added location of ssh keys

--- nixosBase/hosts/nixos/secrets.nix
+++ nixosBase/hosts/nixos/secrets.nix
@@ -1,7 +1,11 @@
 { config, inputs, ... }:
 
 {
-  age = {
+age.identityPaths = [
+    "/home/admin/.ssh/id_ed25519"
+  ];
+  
+age = {
     secrets = {
       secret1 = {
          file = ../../secrets/secret1.age;

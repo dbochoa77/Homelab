b24c67bb9467a195541fca9a64f35137769a8171
dbochoa77 <dbochoa77@proton.me>
2025-08-11 14:37:03 -0700

    added container httpd

---
+++ nixosBase/hosts/nixos/services/containers/httpd/default.nix
@@ -0,0 +1,11 @@
+{
+  containers.httpd = { 
+    autoStart = true;
+    config = {...}: {
+     services.httpd = {
+      enable = true;
+      adminAddr = "foo@example.org";
+      };
+    };
+  };
+}

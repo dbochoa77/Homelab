01653250413b07106c67392b2a7e5aa63db83b75
dbochoa77 <dbochoa77@proton.me>
2025-08-12 14:09:38 -0700

    updated home/admin

---
+++ nixosBase/home/admin/nix/default.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      warn-dirty = false;
+    };
+  };
+}
---
+++ nixosBase/home/admin/overlays/default.nix
@@ -0,0 +1,11 @@
+{ config, outputs, ... }:
+
+{
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.stable-packages
+    ];
+  };
+}
